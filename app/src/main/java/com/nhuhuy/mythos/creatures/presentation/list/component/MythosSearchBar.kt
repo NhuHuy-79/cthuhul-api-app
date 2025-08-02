cf05a34796439ea2a11410cf5de974780c0d105b
Nguyen Nhu Huy <nguyennhuhuy42@gmail.com>
2025-08-02 13:31:46 +0700

    fix: fix retry function and ListViewModel.kt style: style ErrorSection.kt

--- app/src/main/java/com/nhuhuy/mythos/creatures/presentation/list/component/MythosSearchBar.kt
+++ app/src/main/java/com/nhuhuy/mythos/creatures/presentation/list/component/MythosSearchBar.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
+
 @Composable
 fun MythosSearchBar(
     onSearchCancel: () -> Unit,
